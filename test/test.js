ac87b2eb98fbb0840e29d713122731d92741f727
Mark Watson <mark.watson.x@gmail.com>
2018-01-22 13:25:59 -0600

    Headless browser testing

--- test/test.js
+++ test/test.js
@@ -1,6 +1,4 @@
-// var shoppinglist = require('../js/shoppinglist')
-// var shoppinglistModel = require('../js/shoppinglist.model')
-var assert = require('assert');
+// TODO: add real tests
 describe('test', function() {
   it('should return 1', function() {
     assert.equal(1, 1);
